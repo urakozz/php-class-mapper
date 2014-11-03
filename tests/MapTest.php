24d8bbcde787f57db803f2cde0b3fe25690c33e3
Yury Kozyrev <ykmship@yandex-team.ru>
2014-11-03 17:42:31 +0300

    tests

--- tests/MapTest.php
+++ tests/MapTest.php
@@ -36,5 +36,24 @@ class MapTest extends PHPUnit_Framework_TestCase
     $this->assertInternalType('integer', $map->order_warehouse);
   }
 
+  public function testNormalizer()
+  {
+    $map = new MapFixture();
+    $mapper = new Normalizer(new ClassMapper($map));
+    $mapper->setAttributes([
+      'client_id'=>1,
+      'sender_id'=>2,
+      'order_requisite'=>3,
+      'order_warehouse'=>4,
+    ]);
 
+    $this->assertEquals(1, $map->client_id);
+    $this->assertInternalType('integer', $map->client_id);
+    $this->assertEquals(2, $map->sender_id);
+    $this->assertInternalType('integer', $map->sender_id);
+    $this->assertEquals('3', $map->order_requisite);
+    $this->assertInternalType('string', $map->order_requisite);
+    $this->assertEquals('4', $map->order_warehouse);
+    $this->assertInternalType('string', $map->order_warehouse);
+  }
 }
